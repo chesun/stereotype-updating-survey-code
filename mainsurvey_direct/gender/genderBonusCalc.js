2e98660f9d127d7c57f2295f7ae4595864ee42a0
chesun <che.sun.1996@gmail.com>
2021-09-24 22:15:51 -0700

    Update genderBonusCalc.js
    updated code to calculate gender treatment bonus

--- mainsurvey_direct/gender/genderBonusCalc.js
+++ mainsurvey_direct/gender/genderBonusCalc.js
@@ -28,44 +28,121 @@ var baseFee = parseInt("${e://Field/baseFee}");
 if (payQuestionNum==1) {
   //if Q1 is selected for payment, trueStateManHigher takes value 1 if top man score is higher than top woman score in Group A
   trueStateManHigher = parseInt("${e://Field/topmanhigher5050}");
+  var payQuestionBelief = beliefReports[0]; //stated belief in the question
+  var payEventText = "the top scorer in Group A is a man"; //text injection for the event description
+  var payQuestionGroup = "Group A"; //text injection for the group in the question
     if (randomN <= beliefReports[0]) {
       //if randomN is smaller or equal to stated belief in the selected question, use event lottery method
       var earnBonus = (trueStateManHigher == 1) ? 1 : 0; // dummy for whether the subject earns a bonus
-      var bonusAmount = (earnBonus == 1) ? bonusFee : 0; // amount of bonus earned
+      var bonusAmountEarned = (earnBonus == 1) ? bonusFee : 0; // amount of bonus earned
+      var randomNReportCompare = "less than or equal to"; //text injection for comparison between N and stated belief
+      var payMethod = "EL"; //paymnent method, event lottery or number lottery
+      var payMethodText = "Payment on Event"; //text injection for payment method
+        if (trueStateManHigher == 1) {
+          var trueEventText = "the top scorer is a man"; //text injection for the true event
+        } else {
+          var trueEventText = "the top scorer is a woman";
+        }
     } else {
       //if randomN is larger than stated belief, then use number lottery method to calculate payment
       var earnBonus = (numLottRandDraw <= randomN) ? 1 : 0;
-      var bonusAmount = (earnBonus == 1) ? bonusFee : 0;
+      var bonusAmountEarned = (earnBonus == 1) ? bonusFee : 0;
+      var randomNReportCompare = "greater than";
+      var payMethod = "NL"; //payment method is number lottery
+      var payMethodText = "Payment on Lottery";
+        if numLottRandDraw <= randomN {
+          var numLottCompare = "less than or equal to"; //text injection to compare numLottRandDraw and randomN
+          var winLottText = "won"; //text injection for whether subject won or did not win the number lottery
+        } else {
+          var numLottCompare = "greater than";
+          var winLottText = "did not win";
+        }
     }
 
 } else {
       if (payQuestionNum==2) {
         trueStateManHigher = parseInt("${e://Field/avgmanhigher5050}");
+        var payQuestionBelief = beliefReports[1];
+        var payEventText = "the average scoore of the men is higher than the average score of the women in Group A ";
+        var payQuestionGroup = "Group A";
           if (randomN <= beliefReports[1]) {
             //if randomN is smaller or equal to stated belief in the selected question, use event lottery method
             var earnBonus = (trueStateManHigher == 1) ? 1 : 0; // dummy for whether the subject earns a bonus
-            var bonusAmount = (earnBonus == 1) ? bonusFee : 0; // amount of bonus earned
+            var bonusAmountEarned = (earnBonus == 1) ? bonusFee : 0; // amount of bonus earned
+            var randomNReportCompare = "less than or equal to"; //text injection for comparison between N and stated belief
+            var payMethod = "EL"; //paymnent method, event lottery or number lottery
+            var payMethodText = "Payment on Event"; //text injection for payment method
+              if (trueStateManHigher == 1) {
+                var trueEventText = "the average score of the men is higher than the average score of the women";
+              } else {
+                var trueEventText = "the average score of the men is lower than the average score of the women";
+              }
           } else {
             //if randomN is larger than stated belief, then use number lottery method to calculate payment
             var earnBonus = (numLottRandDraw <= randomN) ? 1 : 0;
-            var bonusAmount = (earnBonus == 1) ? bonusFee : 0;
+            var bonusAmountEarned = (earnBonus == 1) ? bonusFee : 0;
+            var randomNReportCompare = "greater than";
+            var payMethod = "NL"; //payment method is number lottery
+            var payMethodText = "Payment on Lottery";
+              if numLottRandDraw <= randomN {
+                var numLottCompare = "less than or equal to"; //text injection to compare numLottRandDraw and randomN
+                var winLottText = "won"; //text injection for whether subject won or did not win the number lottery
+              } else {
+                var numLottCompare = "greater than";
+                var winLottText = "did not win";
+              }
           }
       } else {
+          //For all questions after Q1 and Q2, it's about Group B and top scorer
           trueStateManHigher = parseInt("${e://Field/manhigher}");
             for (let i=3; i<=9; i++) {
               if (payQuestionNum==i) {
+                var payQuestionBelief = beliefReports[i-1];
+                var payEventText = "the top scorer in Group B is a man";
+                var payQuestionGroup = "Group B";
                 if (randomN <= beliefReports[i-1]) {
                   //if randomN is smaller or equal to stated belief in the selected question, use event lottery method
                   // dummy for whether the subject earns a bonus
                   var earnBonus = (trueStateManHigher == 1) ? 1 : 0;
                   // amount of bonus earned
-                  var bonusAmount = (earnBonus == 1) ? bonusFee : 0;
+                  var bonusAmountEarned = (earnBonus == 1) ? bonusFee : 0;
+                  var randomNReportCompare = "less than or equal to"; //text injection for comparison between N and stated belief
+                  var payMethod = "EL"; //paymnent method, event lottery or number lottery
+                  var payMethodText = "Payment on Event"; //text injection for payment method
+                    if (trueStateManHigher == 1) {
+                      var trueEventText = "the top scorer is a man"; //text injection for the true event
+                    } else {
+                      var trueEventText = "the top scorer is a woman";
+                    }
                 } else {
                    //if randomN is larger than stated belief, then use number lottery method to calculate payment
                    var earnBonus = (numLottRandDraw <= randomN) ? 1 : 0;
-                   var bonusAmount = (earnBonus == 1) ? bonusFee : 0;
+                   var bonusAmountEarned = (earnBonus == 1) ? bonusFee : 0;
+                   var randomNReportCompare = "greater than";
+                   var payMethod = "NL"; //payment method is number lottery
+                   var payMethodText = "Payment on Lottery";
+                     if numLottRandDraw <= randomN {
+                       var numLottCompare = "less than or equal to"; //text injection to compare numLottRandDraw and randomN
+                       var winLottText = "won"; //text injection for whether subject won or did not win the number lottery
+                     } else {
+                       var numLottCompare = "greater than";
+                       var winLottText = "did not win";
+                     }
                 }
               }
             }
       }
 }
+
+
+Qualtrics.SurveyEngine.setEmbeddedData("payQuestionBelief",payQuestionBelief);
+Qualtrics.SurveyEngine.setEmbeddedData("payEventText",payEventText);
+Qualtrics.SurveyEngine.setEmbeddedData("payQuestionGroup",payQuestionGroup);
+Qualtrics.SurveyEngine.setEmbeddedData("earnBonus",earnBonus);
+Qualtrics.SurveyEngine.setEmbeddedData("bonusAmountEarned",bonusAmountEarned);
+Qualtrics.SurveyEngine.setEmbeddedData("randomNReportCompare",randomNReportCompare);
+Qualtrics.SurveyEngine.setEmbeddedData("payMethod",payMethod);
+Qualtrics.SurveyEngine.setEmbeddedData("payMethodText",payMethodText);
+Qualtrics.SurveyEngine.setEmbeddedData("trueEventText",trueEventText);
+Qualtrics.SurveyEngine.setEmbeddedData("numLottCompare",numLottCompare);
+Qualtrics.SurveyEngine.setEmbeddedData("winLottText",winLottText);
